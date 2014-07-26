7d2d065b163205a72b49b7646ea27301e325f1ae
Jeff Buttars <jeffbuttars@gmail.com>
2014-07-26 13:12:58 -0600

    Update the src/Makefile and src/setup.py for django-futures.

--- src/setup.py
+++ src/setup.py
@@ -5,14 +5,14 @@ import setuptools
 from setuptools import setup, find_packages
 
 
-setup(name='django-tornado',
-      version='2.0.0a',
-      description="Django/Tornado integration made easy.",
+setup(name='django-futures',
+      version='0.1.0.0',
+      description="Django asynchronous views and Tornado integration made easy.",
       author="Jeff Buttars",
       author_email="jeffbuttars@gmail.com",
       packages=find_packages(),
       license='MIT',
-      package_dir={'django_tornado': 'django_tornado'},
+      package_dir={'django_futures': 'django_futures'},
       install_requires=[
           'tornado',
           'Django',
