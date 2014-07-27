3317838f98658ec3e1e1f5b020b9a22524b61037
Jeff Buttars <jeffbuttars@gmail.com>
2014-07-26 19:33:05 -0600

    Added some more doc text to audit and decorators.

--- src/django_futures/audit.py
+++ src/django_futures/audit.py
@@ -4,12 +4,18 @@
 ``autit.py`` -- Request and Task Auditing
 ===============================================
 
-Enable auditing of decorators, what decorators run and HttpClient ops.
+*TODO*
 
-Allow for realtime and delayed group commits. 
-Realtime being every audit message is commited as it's created. 
-Delayed/grouped allowing multiple audit messages to be commited at once after
-a given period of time.
+Enable auditing of tasks and requests.
 
-Initially support DB audit trail and log file based audit trail.
+* track what tasks/requests run
+* track when tasks/requests run
+* track all HttpClient ops
+* Support DB datastore
+* Support log file/journal datastore
+* Support remote datastore (HTTP, ZeroMQ, AMQP, etc.)
+* allow for realtime and delayed group commits to local datastore
+* allow for realtime and delayed group messaging to a remote store
+    * Realtime being every audit message is commited as it's created.
+    * Delayed/grouped allowing multiple audit messages to be commited at once after a given period of time.
 """
