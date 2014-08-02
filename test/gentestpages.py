06d07e0b73ea8688eef4085427e709a7b266fc58
Jeff Buttars <jeffbuttars@gmail.com>
2014-08-02 15:08:54 -0600

    A new gentestdata that creates more defined, smaller data set

--- test/gentestpages.py
+++ test/gentestpages.py
@@ -3,7 +3,7 @@
 # 2014-07-31 10:58:58.552991
 
 """
-8B 16B 32B 64B 128B 256B 512B
+8B 16B 32B 64B 128B 256B 512B 768B
 1KB 2KB 3KB 4KB 5KB 6KB 7KB 8KB 9KB 
 10KB 20KB 30KB 40KB 50KB 60KB 70KB 80KB 90KB 
 100KB 200KB 300KB 400KB 500KB 600KB 700KB 800KB 900KB 
@@ -14,6 +14,35 @@
 
 import hashlib
 import os
+import json
+import datetime
+
+FMAPS = [
+    {
+        'list': [8, 16, 32, 64, 128, 256, 768],
+    },
+    {
+        'start': 1024,
+        # 'skip': 1024, #  if skip is not same as start, set skip
+        'num': 9,
+    },
+    {
+        'start': 1024 * 10,
+        'num': 9,
+    },
+    {
+        'start': 1024 * 100,
+        'num': 9,
+    },
+    {
+        'start': 1024 * 1024,
+        'num': 9,
+    },
+    {
+        'start': 1024 * 1024 * 10,
+        'num': 10,
+    },
+]
 
 
 def touchopen(fname, *args, **kwargs):
@@ -22,11 +51,52 @@ def touchopen(fname, *args, **kwargs):
 # touchopen()
 
 
+def write_bytes(num_bytes, fbase):
+    print("write_bytes", num_bytes, fbase)
+
+    fn = "%s.txt" % num_bytes
+    fname = os.path.join(fbase, fn)
+    digest = ''
+
+    with touchopen(fname, 'r+b') as fd:
+        rbytes = os.urandom(num_bytes)
+        fd.write(rbytes)
+        h = hashlib.sha256()
+        h.update(rbytes)
+        digest = h.hexdigest()
+
+    if os.path.getsize(fname) != num_bytes:
+        raise Exception("Invalid file length")
+
+    return {'file_name': fn, 'sha256': digest}
+# write_bytes()
+
+
+def write_skipped(fmap, fbase):
+    start = fmap['start']
+    skip = fmap.get('skip', start)
+    stop = start * (fmap['num'] + 1)
+
+    result_list = []
+    for blen in range(start, stop, skip):
+        result_list.append(write_bytes(blen, fbase))
+    # end for blen in range(fmap['start'], )
+
+    return result_list
+# write_skipped()
+
+
+def write_listed(fmap, fbase):
+    result_list = []
+    for blen in fmap['list']:
+        result_list.append(write_bytes(blen, fbase))
+    # end for blen in fmap['list']
+
+    return result_list
+# write_listed()
+
+
 def main():
-    start_bytes = 1024
-    end_bytes = 10 * 1024 * 1024  # 10 MB
-    # end_bytes = 10 * 1024  # 10 KB
-    skip_bytes = 1024
     out_dir = 'data_pages'
 
     try:
@@ -34,24 +104,24 @@ def main():
     except FileExistsError:
         pass
 
-    for blen in range(start_bytes, end_bytes + 1, skip_bytes):
-        fname = "%s.txt" % blen
-        fname = os.path.join(out_dir, fname)
-        # print("FNAME", blen, fname)
+    result_list = []
+    for fmap in FMAPS:
+        if 'list' in fmap:
+            result_list += write_listed(fmap, out_dir)
+        elif 'start' in fmap:
+            result_list += write_skipped(fmap, out_dir)
+        else:
+            raise Exception("Bad file map %s" % fmap)
 
-        with touchopen(fname, 'r+b') as fd:
-            fd.write(os.urandom(blen))
-            # fd.seek(0)
-            h = hashlib.sha256()
-            h.update(fd.read(blen))
-            digest = h.digest()
-            # print("DIGEST", len(digest), digest)
-            fd.write(b'\x0D' + bytes(digest))
+    fname = os.path.join(out_dir, 'file_list.json')
+    file_data = {
+        'timestamp': datetime.datetime.now().isoformat(),
+        'file_list': result_list,
+    }
 
-        print("FNAME {}:{}".format(fname, os.path.getsize(fname)))
-        if os.path.getsize(fname) != blen + 33:
-            raise Exception("Invalid file length")
-    # end for blen in range(start_bytes, end_bytes + 1)
+    with open(fname, 'w') as fd:
+        fd.write(json.dumps(file_data, sort_keys=True, indent=2))
+    # end for fmap in FMAPS
 # main()
 
 
